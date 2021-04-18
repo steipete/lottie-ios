68f306465388b9930f0475678ca449c920bdfc09
Peter Steinberger <steipete@gmail.com>
2021-04-18 02:08:02 +0200

    Change to LottieMac

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     platforms: [.macOS("10.10")],
     // platforms: [.iOS("9.0"), .macOS("10.10"), tvOS("9.0"), .watchOS("2.0")],
     products: [
-        .library(name: "Lottie", targets: ["Lottie"])
+        .library(name: "LottieMac", targets: ["Lottie"])
     ],
     targets: [
         .target(
